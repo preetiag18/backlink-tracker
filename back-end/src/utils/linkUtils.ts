f391a20b8c4fd7f7498e65499079c962669ce354
Preeti Agrawal <preeti.agrawal@tutors.es>
2023-09-09 16:45:54 +0300

    add more parameters for puppeteer

--- back-end/src/utils/linkUtils.ts
+++ back-end/src/utils/linkUtils.ts
@@ -13,6 +13,12 @@ export const getLinkStatus = async (
       args: [
           "--no-sandbox",
           "--disable-gpu",
+          "--disable-setuid-sandbox",
+          "--disable-dev-shm-usage",
+          "--disable-accelerated-2d-canvas",
+          "--no-first-run",
+          "--no-zygote",
+          "--single-process"
       ]
     });
     const page = await browser.newPage();
